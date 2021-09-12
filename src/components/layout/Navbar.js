25cb6b5a31625e4d2700e6fd9f13622e4a35a3cd
Kim Carpico <kcarpico@gmail.com>
2021-09-12 16:51:14 -0700

    converted class components to functional components

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,29 +1,54 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+import React from "react";
 // impt -> shortcut
 import PropTypes from "prop-types";
 
-export class Navbar extends Component {
-  // *** SETS DEFAULT PROPS ***
-  static defaultProps = {
-    title: "Github Finder",
-    icon: "fab fa-github",
-  };
+// export class Navbar extends Component {
+//   // *** SETS DEFAULT PROPS ***
+//   static defaultProps = {
+//     title: "Github Finder",
+//     icon: "fab fa-github",
+//   };
 
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-  };
+//   static propTypes = {
+//     title: PropTypes.string.isRequired,
+//     icon: PropTypes.string.isRequired,
+//   };
 
-  render() {
-    return (
-      <nav className="navbar bg-primary">
-        <h1>
-          <i className={this.props.icon} /> {this.props.title}
-          Navbar
-        </h1>
-      </nav>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <nav className="navbar bg-primary">
+//         <h1>
+//           <i className={this.props.icon} /> {this.props.title}
+//           Navbar
+//         </h1>
+//       </nav>
+//     );
+//   }
+// }
+
+// const Navbar = (props) => {
+
+const Navbar = ({ icon, title }) => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        {/* <i className={props.icon} /> {props.title} */}
+        <i className={icon} /> {title}
+      </h1>
+    </nav>
+  );
+};
+
+// *** SETS DEFAULT PROPS ***
+Navbar.defaultProps = {
+  title: "Github Finder",
+  icon: "fab fa-github",
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
 
 export default Navbar;
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,41 +1,68 @@
 // rce -> shortcut
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-class UserItem extends Component {
-  //   constructor() {
-  //     super();
-  //     this.state = {
-  //       id: "id",
-  //       login: "mojombo",
-  //       avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-  //       html_url: "https://github.com/mojombo",
-  //     };
-  // }
+import React from "react";
+// impt -> shortcut
+import PropTypes from "prop-types";
 
-  // No Longer Using state, we are using Props instead because this is what the USER is being passed in as
-  //   state = {
-  //     id: "id",
-  //     login: "mojombo",
-  //     avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-  //     html_url: "https://github.com/mojombo",
-  //   };
+// *** CLASS COMPONENTS ***
+// class UserItem extends Component {
+//   //   constructor() {
+//   //     super();
+//   //     this.state = {
+//   //       id: "id",
+//   //       login: "mojombo",
+//   //       avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+//   //       html_url: "https://github.com/mojombo",
+//   //     };
+//   // }
 
-  render() {
-    // destructured to get rid of the 'this.state.' in front of line 28 and 31
-    const { login, avatar_url, html_url } = this.props.user;
+//   // No Longer Using state, we are using Props instead because this is what the USER is being passed in as
+//   //   state = {
+//   //     id: "id",
+//   //     login: "mojombo",
+//   //     avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+//   //     html_url: "https://github.com/mojombo",
+//   //   };
 
-    return (
-      <div className="card text-center">
-        <img src={avatar_url} className="round-img" style={{ width: "60px" }} />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">
-            More
-          </a>
-        </div>
+//   render() {
+//     // destructured to get rid of the 'this.state.' in front of line 28 and 31
+//     const { login, avatar_url, html_url } = this.props.user;
+
+//     return (
+//       <div className="card text-center">
+//         <img src={avatar_url} className="round-img" style={{ width: "60px" }} />
+//         <h3>{login}</h3>
+//         <div>
+//           <a href={html_url} className="btn btn-dark btn-sm my-1">
+//             More
+//           </a>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// const UserItem = (props) => {
+//   const { login, avatar_url, html_url } = props.user;
+
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className="card text-center">
+      <img src={avatar_url} className="round-img" style={{ width: "60px" }} />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className="btn btn-dark btn-sm my-1">
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  // pta => shortcut
+  user: PropTypes.object.isRequired,
+};
 
 export default UserItem;
