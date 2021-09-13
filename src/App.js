51ca288bd6e236dfb4fe983ab30971c0e2bc88dd
Kim Carpico <kcarpico@gmail.com>
2021-09-12 17:23:47 -0700

    HTTP requests and updating State

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+
+// import axios
+import axios from "axios";
+
 import "./App.css";
 
 class App extends Component {
+  state = {
+    users: [],
+    // REASON FOR LOADING: moment before data is fetched
+    loading: false,
+  };
+
+  // lifecycle method
+  // *** USING PROMISES ***
+  // USE THIS FOR WHEN YOU WANT A COMPONENT TO FIRE WHEN 1ST STARTING UP using Axios
+  // componentDidMount() {
+  //   axios
+  //     .get("https://api.github.com/users")
+  //     .then((res) => console.log(res.data));
+  // }
+
+  // *** USING ASYNC/AWAIT ***
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get("https://api.github.com/users");
+    // console.log(res.data);
+    // Want to reset state
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     const numbers = [1, 2, 3, 4];
 
@@ -13,7 +42,7 @@ class App extends Component {
         {/* <Navbar title={numbers} /> */}
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
